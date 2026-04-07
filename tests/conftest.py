1af9ffb57ed55f4f1f0c3cb91750944a5e4852c5
ehe-cetic <erwan.henin@cetic.be>
2026-04-07 10:30:46 +0200

    Update CI CD

--- tests/conftest.py
+++ tests/conftest.py
@@ -48,7 +48,7 @@ def driver():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
-
+    chrome_options.binary_location = "/usr/bin/chromium-browser"
     # Enable browser logging
     chrome_options.set_capability("goog:loggingPrefs", {"browser": "ALL"})
 
